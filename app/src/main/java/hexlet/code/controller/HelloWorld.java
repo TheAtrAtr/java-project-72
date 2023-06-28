939bc57d009a2c266f3748ece9603f71077b1cae
TheAtrAtr <dyadya_atr@mail.ru>
2023-06-28 20:13:40 +0300

    step4

--- app/src/main/java/hexlet/code/controller/HelloWorld.java
+++ app/src/main/java/hexlet/code/controller/HelloWorld.java
@@ -6,8 +6,5 @@ import io.javalin.http.Handler;
 import java.io.PrintWriter;
 
 public class HelloWorld {
-    public static Handler helloWorld = ctx -> {
-        PrintWriter writer = ctx.res().getWriter();
-        writer.write("Hello World");
-    };
+    public static Handler helloWorld = ctx -> ctx.render("index.html");
 }
